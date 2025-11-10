b06f0864eb5408789ed5d7b410b2b61d941b2a43
Scharek <Vera.Scharek@bam.de>
2025-11-10 10:43:32 +0100

    added testing of R_m value and fixed test of warning message

--- tests/testthat/test-get_isoratio.R
+++ tests/testthat/test-get_isoratio.R
@@ -5,7 +5,7 @@ testthat::test_that(
       ldply_base(1:length(x), function(i) {
         get_isoratio(
           x[[i]], iso1_col = "113Cd", iso2_col = "111Cd", PPmethod = "Peak (manual)",
-          peak_start = 72, peak_end = 132
+          peak_start = 70, peak_end = 132
         )
       })
     })
@@ -16,21 +16,15 @@ testthat::test_that(
 
     testthat::expect_equal(colnames(iso_peaks[[1]]), c("Spike isotope","Sample isotope","R_m"))
     testthat::expect_true(is.numeric(iso_peaks[[1]][,"R_m"]))
+    testthat::expect_equal(iso_peaks[[2]][3,"R_m"], c(1.004), tolerance = 0.01)
   }
 )
 
 testthat::test_that(
   desc = "Test that get_isoratio warning message works",
   code = {
-    test <-
-        get_isoratio(
-          ETVapp::ETVapp_testdata[["oIDMS"]][[2]][[2]], iso1_col = "117Sn", iso2_col = "122Sn", PPmethod = "Peak (manual)",
-          peak_start = 70, peak_end = 105
-        )
-      }
-# $$VS: Should return an warning message, if peak picking method "Peak (height)" would work.
-#       Code is changed but peak boundaries don't change according to minpeakheight.
     testthat::expect_warning(get_isoratio(
-      ETVapp::ETVapp_testdata[["oIDMS"]][[2]][[2]], iso1_col = "117Sn", iso2_col = "122Sn", PPmethod = "Peak (height)", minpeakheight = 10000
+      ETVapp::ETVapp_testdata[["oIDMS"]][[2]][[2]], iso1_col = "117Sn", iso2_col = "122Sn", PPmethod = "Peak (height)", minpeakheight = 100000
     ))
+    }
 )
